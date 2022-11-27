bc93374d559d6a993dca794fc2c596d32d365677
Benedetto Simone <72497905+BenedettoSimone@users.noreply.github.com>
2022-11-27 12:47:56 +0100

    Add files via upload

---
+++ single_sections_code/StreetLights/StreetLights.ino
@@ -0,0 +1,47 @@
+/* Street lights program. */
+
+const int LED_PIN_1 = 12;
+const int LED_PIN_2 = 8;
+
+void setup() {
+
+  Serial.begin(9600); // initialize serial communication at 9600 bits per second
+  pinMode(LED_PIN_1, OUTPUT);
+  pinMode(LED_PIN_2, OUTPUT);
+}
+
+void loop() {
+  
+  // reads the input on analog pin A3 (value between 0 and 1023)
+  int analogValueEntry = analogRead(A3);
+
+  Serial.print("Analog reading entry: ");
+  Serial.print(analogValueEntry);   // the raw analog reading
+
+  /*
+  // We'll have a few threshholds, qualitatively determined
+  if (analogValueEntry < 10) {
+    Serial.println(" - Dark");
+  } else if (analogValueEntry < 200) {
+    Serial.println(" - Dim");
+  } else if (analogValueEntry < 500) {
+    Serial.println(" - Light");
+  } else if (analogValueEntry < 800) {
+    Serial.println(" - Bright");
+  } else {
+    Serial.println(" - Very bright");
+  }
+
+  */
+
+  if (analogValueEntry < 200){
+    // Dark
+    digitalWrite(LED_PIN_1, HIGH);
+    digitalWrite(LED_PIN_2, HIGH);
+  } else {
+    digitalWrite(LED_PIN_1, LOW);
+    digitalWrite(LED_PIN_2, LOW);
+  }
+  
+  delay(1000);
+}
